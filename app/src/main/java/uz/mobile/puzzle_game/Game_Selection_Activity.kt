9eee5a3126ae80e63f7cab634daa43b5ce37d3fc
Puzzle_Game <abdumaliknuraliyev0@gmail.com>
2022-10-05 19:16:35 +0500

    Puzzle

---
+++ app/src/main/java/uz/mobile/puzzle_game/Game_Selection_Activity.kt
@@ -0,0 +1,13 @@
+package uz.mobile.puzzle_game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Game_Selection_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_selection)
+
+        var a = 40
+    }
+}

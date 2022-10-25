2462220e7ebf9b606c980daa1f1fda900f10e8b5
Puzzle_Game <abdumaliknuraliyev0@gmail.com>
2022-10-25 16:14:23 +0500

    Puzzle

--- app/src/main/java/uz/mobile/puzzle_game/Game_Selection_Activity.kt
+++ app/src/main/java/uz/mobile/puzzle_game/Game_Selection_Activity.kt
@@ -1,13 +1,101 @@
 package uz.mobile.puzzle_game
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import uz.mobile.puzzle_game.databinding.ActivityGameSelectionBinding
+import java.sql.Time
+import java.util.*
 
 class Game_Selection_Activity : AppCompatActivity() {
+    private lateinit var binding: ActivityGameSelectionBinding
+    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var shared: SharedPreferences
+    private var music: String = ""
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game_selection)
+        binding = ActivityGameSelectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadAnimations()
+        shared = getSharedPreferences("game", Context.MODE_PRIVATE)
+        val gameState = shared.getString("gameState","")
+
+
+        binding.resume.setOnClickListener{
+            getStateGame()
+        }
+
+        binding.settings.setOnClickListener {
+            startActivity(Intent(this,Settings::class.java))
+        }
+        binding.restart.setOnClickListener {
+            startActivity(Intent(this,Selection_Game::class.java))
+        }
+        getMusicPlayerState()
 
 
     }
+
+    private fun getStateGame() {
+        shared = getSharedPreferences("game", Context.MODE_PRIVATE)
+        val gameState = shared.getString("gameState","")
+        if (gameState=="Image_Puzzle"){
+            startActivity(
+                Intent(
+                    this,
+                    Image_Puzzle::class.java
+                )
+            )
+
+        }
+        if (gameState=="Number_Puzzle"){
+            startActivity(
+                Intent(
+                    this,
+                    Number_Puzzle::class.java
+                )
+            )
+
+        }
+        if (gameState=="Slide_Puzzle"){
+            startActivity(
+                Intent(
+                    this,
+                    Slide_Puzzle::class.java
+                )
+            )
+
+        }
+
+    }
+
+    private fun loadAnimations() {
+        val _left_move = AnimationUtils.loadAnimation(this, R.anim.left_move)
+        val _right_move = AnimationUtils.loadAnimation(this, R.anim.righ_move)
+        binding.resume.startAnimation(_left_move)
+        binding.settings.startAnimation(_left_move)
+        binding.restart.startAnimation(_right_move)
+
+    }
+     fun getMusicPlayerState() {
+         shared = getSharedPreferences("settings", Context.MODE_PRIVATE)
+         if ("off" == shared.getString("isPlaying", "").toString()) {
+             shared = getSharedPreferences("settings", Context.MODE_PRIVATE)
+             val editor: SharedPreferences.Editor = shared.edit()
+             editor.putString("isPlaying", "on")
+             editor.apply()
+             startService(Intent(this, MyService::class.java))
+         }
+
+
+    }
+
+
 }
--- app/src/main/java/uz/mobile/puzzle_game/Splash_Screen.kt
+++ app/src/main/java/uz/mobile/puzzle_game/Splash_Screen.kt
@@ -1,12 +1,38 @@
 package uz.mobile.puzzle_game
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.HandlerCompat.postDelayed
+import uz.mobile.puzzle_game.databinding.ActivitySplashScreenBinding
+
 
 class Splash_Screen : AppCompatActivity() {
+    private lateinit var _binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        loadAnimations()
+        startNextScreen()
+
+
 
     }
+
+    private fun startNextScreen() {
+        Handler().postDelayed({
+            val intent = Intent(this,Game_Selection_Activity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)    }
+
+    private fun loadAnimations() {
+        val _left_move = AnimationUtils.loadAnimation(this, R.anim.left_move)
+        val _right_move = AnimationUtils.loadAnimation(this, R.anim.righ_move)
+        _binding.gameText.startAnimation(_left_move)
+        _binding.puzzleText.startAnimation(_right_move)    }
 }
